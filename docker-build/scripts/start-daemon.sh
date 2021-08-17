fab0f86d6a0bee658f382c5ab90d9bd5ff0d03b6
shoothzj <shoothzj@gmail.com>
2021-08-17 21:01:11 +0800

    cluster init script exit

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nohup $BOOKKEEPER_HOME/mate/bookkeeper_mate >$BOOKKEEPER_HOME/bookkeeper_mate.stdout.log 2>$BOOKKEEPER_HOME/bookkeeper_mate.stderr.log
+if [ $CLUSTER_INIT == "true" ]; then
+    $BOOKKEEPER_HOME/mate/bookkeeper_mate 2>&1
+else
+    nohup $BOOKKEEPER_HOME/mate/bookkeeper_mate >$BOOKKEEPER_HOME/bookkeeper_mate.stdout.log 2>$BOOKKEEPER_HOME/bookkeeper_mate.stderr.log
+fi
