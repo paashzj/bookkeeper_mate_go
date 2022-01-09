967bc165a547d0855f556dff66c10965a1828224
lovehzj <1922919664@qq.com>
2022-01-09 19:00:11 +0800

    fix kernel log path

--- docker-build/scripts/start-bookkeeper.sh
+++ docker-build/scripts/start-bookkeeper.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-$BOOKKEEPER_HOME/bin/bookkeeper bookie >$BOOKKEEPER_HOME/bookkeeper.stdout.log 2>$BOOKKEEPER_HOME/bookkeeper.stderr.log
+mkdir $BOOKKEEPER_HOME/logs
+$BOOKKEEPER_HOME/bin/bookkeeper bookie >>$BOOKKEEPER_HOME/bookkeeper.stdout.log 2>>$BOOKKEEPER_HOME/bookkeeper.stderr.log
+
